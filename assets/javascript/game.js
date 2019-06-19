9715686e8a66ff1ad437dea0d567cf54855f57a6
Michael Passow <passowmichael@yahoo.com>
2019-06-19 15:08:55 -0400

    created basic html/ folders/ added javascript code

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+
+
+
+
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+
+var wins = 0;
+var losses = 0; 
+var remainingGuesses = 7;
+
+var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    console.log(computerChoice)
