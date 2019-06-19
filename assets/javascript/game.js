e61076177463bec918c448c5bdf2e696cb0ccaaa
Michael Passow <passowmichael@yahoo.com>
2019-06-19 16:22:45 -0400

    created letter guessing syntax

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,6 +8,32 @@ var wins = 0;
 var losses = 0; 
 var remainingGuesses = 7;
 
+
+
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
 
     console.log(computerChoice)
+
+//Receives user input then runs the following function
+
+    document.onkeypress = function(event) {
+       
+        var userGuess = event.key;
+    
+        if(userGuess === computerChoice){
+            wins++;
+        }
+        else{
+            remainingGuesses--;
+        }
+    
+        if(remainingGuesses === 0){
+            losses++
+        }
+        
+
+        document.getElementById('wins').innerHTML = "Wins: " + wins;
+        document.getElementById('losses').innerHTML = "losses: " + losses;
+        document.getElementById('remainingGuesses').innerHTML = "Guesses left: " + remainingGuesses;
+        document.getElementById('userGuess').innerHTML = "User letter guessed: " + userGuess;
+    }
