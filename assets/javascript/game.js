c8804d12b208b52445cac759fde58e388ef3f5ab
Michael Passow <passowmichael@yahoo.com>
2019-06-21 22:33:30 -0400

    updated game mechanics, added css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,38 +2,63 @@
 
 
 
-var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// Variables 
 
 var wins = 0;
-var losses = 0; 
+var losses = 0;
 var remainingGuesses = 7;
 
+// Array of the alphabets that computer can guess from
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
+var guessArr = [];  // this array will be used to store the user inputs in userInput paragraph.
 
+// Randomly chooses from letters variable
 var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+console.log(computerChoice);
 
-    console.log(computerChoice)
+// Functions
 
-//Receives user input then runs the following function
+  document.onkeyup = function(event) {
+    var userInput = event.key;
+    var lowerCase = userInput.toLocaleLowerCase(); 
+    guessArr.push(lowerCase);
 
-    document.onkeypress = function(event) {
-       
-        var userGuess = event.key;
-    
-        if(userGuess === computerChoice){
-            wins++;
-        }
-        else{
-            remainingGuesses--;
-        }
-    
-        if(remainingGuesses === 0){
-            losses++
-        }
-        
+    if (lowerCase === computerChoice) {
 
-        document.getElementById('wins').innerHTML = "Wins: " + wins;
-        document.getElementById('losses').innerHTML = "losses: " + losses;
-        document.getElementById('remainingGuesses').innerHTML = "Guesses left: " + remainingGuesses;
-        document.getElementById('userGuess').innerHTML = "User letter guessed: " + userGuess;
+      wins++;
+      resetGame();  // Resets computerChoice and guessCounter
+
+    } else {
+
+      remainingGuesses--;
+
+    };
+
+    if (remainingGuesses < 0) {
+
+      losses++;
+      resetGame();
+
+    };
+
+    if (remainingGuesses === 0) {
+      alert ("You must not be psychic!")
     }
+    
+
+
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
+    document.getElementById("remainingGuesses").innerHTML = "Remaining guesses: " + remainingGuesses;
+    document.getElementById("userInput").innerHTML = "Your guesses so far: " + guessArr;
+   
+  };
+
+
+  function resetGame() {
+    remainingGuesses = 7;
+
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log(computerChoice);
+  };
